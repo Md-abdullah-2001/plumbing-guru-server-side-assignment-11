6298218360c1912e3f4ad3c1510d2aab8578faa1
Md-abdullah-2001 <mdabdullah.dm2001@gmail.com>
2022-11-09 00:31:35 +0600

    created details sevice api and addedmongo database connection

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -14,13 +14,39 @@ app.get("/", (req, res) => {
   res.send("Assignment-11 Server is Running");
 });
 
-const uri = `mongodb+srv://{process.env.DB_USER}:{PROCESS.ENV.DB_PASSWORD}@cluster0.mbnoo25.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mbnoo25.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+async function run() {
+  try {
+    const serviceCollection = client
+      .db("plumbing-assignment")
+      .collection("services");
+
+    // get home page API
+    app.get("/homeservices", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const HomeServices = await cursor.limit(3).toArray();
+      res.send(HomeServices);
+    });
+    // single service data details
+    app.get("/homeservices/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+      const serviceDetail = await serviceCollection.findOne(query);
+      res.send(serviceDetail);
+    });
+  } finally {
+  }
+}
+run().catch((error) => console.log(error));
 
 app.listen(port, () => {
   console.log(`Assignment server is running on ${port}`);
